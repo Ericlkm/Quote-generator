c2ecd8c79a67f329920059ac9c559915bb0baa31
Ericlkm <gtprinceeric.el1@gmail.com>
2024-06-30 01:22:13 -0400

    saving finished Quote generator for now!

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const author = document.querySelector(".author");
 const copy = document.querySelector(".copy");
 const confirmCopy = document.querySelector(".confirm");
 const url = "https://api.quotable.io/random";
+ptag.innerHTML = `
+<span class="major-mono-display-regular" style="font-weight:bolder;" >
+Follow me on <a class="text-decoration-none fa-beat-fade" href="http://github.com/ericlkm" target="_blank" title="my Github">github</a>!!
+</span><hr/><br/>
+Click below to generate quote!<br/><i class="fa-solid fa-arrow-down fa-bounce"></i>
+`;
 
 async function fetchData() {
   const data = await fetch(url);
@@ -17,16 +23,15 @@ async function fetchData() {
 document.getElementById("new-quote").addEventListener("click", () => {
   setTimeout(() => {
     ptag.textContent = `Loading...`;
+    author.innerHTML = "";
     fetchData();
   }, 100);
 });
 
 copy.addEventListener("click", () => {
-  $(".confirm").fadeIn(500);
   navigator.clipboard.writeText(ptag.innerHTML);
   confirmCopy.textContent = "Copied";
   setTimeout(() => {
     $(".confirm").fadeOut(1000);
   }, 1000);
 });
-fetchData();
